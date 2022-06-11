ebe1abebed69d97aefc73a972481d7e32f2962b9
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2022-06-11 10:45:17 -0500

    something

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Carrousel } from './components/Carrousel/Carrousel';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><strong>COLORS</strong> Video Picker </h1>
+        <Carrousel />
+        {/* Creates a rgb slider */}
+        <div className="slider-container">
+          <div className="slider-label">Red</div>
+          <input type="range" min="0" max="255" value="0" step="1" className="slider" id="red" />
+        </div>
+        <div className="slider-container">
+          <div className="slider-label">Green</div>
+          <input type="range" min="0" max="255" value="0" step="1" className="slider" id="green" />
+        </div>
+        <div className="slider-container">
+          <div className="slider-label">Blue</div>
+          <input type="range" min="0" max="255" value="0" step="1" className="slider" id="blue" />
+        </div>
+        <input type="color" id="colorPicker" />
       </header>
     </div>
   );
