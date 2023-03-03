d8247553e56fe5a5baa6ed89938298c60d98b81f
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-03-02 18:12:07 -0600

    Fix color picking

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,17 +3,31 @@ import '../styles/App.css'
 
 function App () {
   const [background, setBackground] = useState('eeaeca')
+  const [backgroundDecimal, setBackgroundDecimal] = useState(0)
 
   useEffect(() => {
-    setBackground(` radial-gradient(circle, #${background} 90%, #f7f7f7 100%)`)
+    setBackground(`linear-gradient(#${background} 60%, white)`)
   }, [])
+
+  const onChangeRangeValue = (value) => {
+    setBackgroundDecimal(value)
+
+    setBackground(`linear-gradient(#${value} 60%, white)`)
+  }
+
   return (
     <div className="App" style={{ background }}>
       <main className='main'>
         <h1 class="title">COLORS video player</h1>
         <img className="main-video" src="https://i.ytimg.com/vi_webp/vCwKgEFSsyI/maxresdefault.webp" alt="main video" style={{ objectFit: 'contain' }}/>
       </main>
+      <p>Running with {background}</p>
       <article class="container-items">
+          <div class="color-slider">
+            <input type="range" min="0" max="255" value={backgroundDecimal} step={1} class="slider" id="myRange"
+              onChange={(e) => onChangeRangeValue(e.target.value)}
+            />
+          </div>
           <h2 class="subtitle">Latests videos</h2>
           <li class="card"></li>
           <li class="card"></li>
