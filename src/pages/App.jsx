a4457ecd83a171401fdfd14ec37f876ba15878cf
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-04-04 12:40:21 -0600

    Use debounce to wait for color changes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,31 +1,27 @@
 import { useEffect, useState } from 'react'
+import { useDebounce } from '../hooks/useDebounce'
 import '../styles/App.css'
 
 function App () {
-  const [background, setBackground] = useState('eedbae')
-  const [backgroundDecimal, setBackgroundDecimal] = useState('#eedbae')
+  const [colorValue, setColorValue] = useState('#eedbae')
+  const debounceBackgroundDecimal = useDebounce(colorValue, 500)
 
   useEffect(() => {
-    setBackground(`linear-gradient(#${background} 60%, white)`)
-  }, [])
-
-  const onChangeRangeValue = (value) => {
-    setBackgroundDecimal(value)
-
-    setBackground(`linear-gradient(${value} 60%, white)`)
-  }
+    console.log(`El color seleccionado es: ${debounceBackgroundDecimal}`)
+    // TODO: Make request to yt api
+  }, [debounceBackgroundDecimal])
 
   return (
-    <div className="App" style={{ background }}>
+    <div className="App" style={{ background: `linear-gradient(${debounceBackgroundDecimal} 15%, white)` }}>
       <main className='main'>
         <h1 class="title">COLORS video player</h1>
         <img className="main-video" src="https://i.ytimg.com/vi_webp/vCwKgEFSsyI/maxresdefault.webp" alt="main video" style={{ objectFit: 'contain' }}/>
       </main>
-      <p>Running with {background}</p>
+      <p>Running with {debounceBackgroundDecimal}</p>
       <article class="container-items">
           <div class="color-slider">
-            <input type="color" value={backgroundDecimal} step={1} class="slider" id="myRange"
-              onChange={(e) => onChangeRangeValue(e.target.value)}
+            <input type="color" value={debounceBackgroundDecimal} step={1} className="slider" id="myRange"
+              onChange={(e) => setColorValue(e.target.value)}
             />
           </div>
           <h2 class="subtitle">Latests videos</h2>
