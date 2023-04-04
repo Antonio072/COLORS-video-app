56b3d32060e4bb7ba2641377277e0eb356d4d829
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-04-04 12:13:45 -0600

    Fix responsive layout

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 import '../styles/App.css'
 
 function App () {
-  const [background, setBackground] = useState('eeaeca')
-  const [backgroundDecimal, setBackgroundDecimal] = useState(0)
+  const [background, setBackground] = useState('eedbae')
+  const [backgroundDecimal, setBackgroundDecimal] = useState('#eedbae')
 
   useEffect(() => {
     setBackground(`linear-gradient(#${background} 60%, white)`)
@@ -12,7 +12,7 @@ function App () {
   const onChangeRangeValue = (value) => {
     setBackgroundDecimal(value)
 
-    setBackground(`linear-gradient(#${value} 60%, white)`)
+    setBackground(`linear-gradient(${value} 60%, white)`)
   }
 
   return (
@@ -24,11 +24,37 @@ function App () {
       <p>Running with {background}</p>
       <article class="container-items">
           <div class="color-slider">
-            <input type="range" min="0" max="255" value={backgroundDecimal} step={1} class="slider" id="myRange"
+            <input type="color" value={backgroundDecimal} step={1} class="slider" id="myRange"
               onChange={(e) => onChangeRangeValue(e.target.value)}
             />
           </div>
           <h2 class="subtitle">Latests videos</h2>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
+          <li class="card"></li>
           <li class="card"></li>
           <li class="card"></li>
           <li class="card"></li>
