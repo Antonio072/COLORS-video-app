e25c9d5cb37b444bf16fba28e1356a489bc57ed0
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-04-15 12:01:24 -0600

    Add GH Pages to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/colors-video-app/',
+  base: '/COLORS-video-app/',
   plugins: [react()]
 })
