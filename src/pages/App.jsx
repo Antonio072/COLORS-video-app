bc911f2f1fa3214cdf7187d7cd1dc8ae3720eb15
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-04-21 15:45:28 -0600

    Use contrasting color for texts

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,6 +8,7 @@ function App () {
   const debounceBackgroundDecimal = useDebounce(colorValue, 500)
   const [filteredData, setFilteredData] = useState(data)
   const [currentVideo, setCurrentVideo] = useState({ video_id: 'vCwKgEFSsyI', thumbnail_url: 'https://i.ytimg.com/vi/vCwKgEFSsyI/maxresdefault.jpg', predominant_color: '#9f9f9f' })
+  const [fontContrastColor, setFontContrastColor] = useState('#000000')
 
   useEffect(() => {
     console.log(`El color seleccionado es: ${debounceBackgroundDecimal}`)
@@ -22,7 +23,7 @@ function App () {
       return rDiff <= offset && gDiff <= offset && bDiff <= offset
     })
     console.log(filteredData)
-    setFilteredData(Array.from(new Set(filteredData)))
+    setFilteredData(filteredData)
   }, [debounceBackgroundDecimal])
 
   function hexToRgb (hexValue) {
@@ -35,19 +36,39 @@ function App () {
 
   const handleChangeCurrentVideo = (item) => {
     setCurrentVideo(item)
+    const contrastingColor = getContrastingColor(item.predominant_color)
+    setFontContrastColor(contrastingColor)
+    console.log(contrastingColor)
+  }
+
+  function getContrastingColor (backgroundColor) {
+    let r = parseInt(backgroundColor.substring(1, 3), 16)
+    let g = parseInt(backgroundColor.substring(3, 5), 16)
+    let b = parseInt(backgroundColor.substring(5, 7), 16)
+
+    r = 255 - r
+    g = 255 - g
+    b = 255 - b
+
+    r = r.toString(16).padStart(2, '0')
+    g = g.toString(16).padStart(2, '0')
+    b = b.toString(16).padStart(2, '0')
+
+    const contrastingColor = '#' + r + g + b
+    return contrastingColor
   }
 
   return (
     <div className="App" style={{ background: `linear-gradient(${currentVideo.predominant_color} 35%, white)` }}>
       <main className='main'>
-        <h1 class="title">COLORS video player</h1>
+        <h1 class="title" style={{ color: fontContrastColor }}>COLORS video player</h1>
         <iframe className="main-video"
                 src={`https://www.youtube.com/embed/${currentVideo.video_id}`}
                 title="YouTube video player"
                  frameborder="0"
                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                  allowfullscreen/>
-        <h2 className='subtitle'>Playing: {currentVideo.title}</h2>
+        <h2 className='subtitle' style={{ color: fontContrastColor }}>Playing: {currentVideo.title}</h2>
       </main>
       <article class="container-items">
         <div class="color-slider">
@@ -55,7 +76,12 @@ function App () {
             onChange={(e) => setColorValue(e.target.value)}
           />
         </div>
-        <h2 className="subtitle">Latest videos</h2>
+        <h2 className="subtitle" style={{ color: fontContrastColor }}>
+          {filteredData.length === 0
+            ? 'No videos found'
+            : 'Latest videos'
+          }
+        </h2>
         {filteredData.map((item) =>
           <button id={item.video_id} className="card" onClick={() => handleChangeCurrentVideo(item)}>
             <img src={item.thumbnail_url} alt="thumbnail" className="thumbnail"/>
