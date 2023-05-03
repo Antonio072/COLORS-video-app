5d559cc8a8d25a3866bd56bde5107ee35502bb0e
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-05-03 16:41:32 -0600

    feat: Add Google Analytics

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/COLORS-video-app/',
-  plugins: [react()]
+  plugins: [react(),
+    VitePluginRadar({
+      enableDev: true, // enable radar in dev mode
+      analytics: [
+        {
+          id: 'G-160XC9TQ5X',
+          config: {
+            cookie_domain: 'auto',
+            cookie_expires: 63072000,
+            cookie_prefix: 'none',
+            cookie_update: true,
+            cookie_flags: '',
+            send_page_view: true,
+            allow_google_signals: true,
+            allow_ad_personalization_signals: true
+          },
+
+          persistentValues: {
+            currency: 'MXN'
+          }
+        }
+      ]
+
+    })]
 })
